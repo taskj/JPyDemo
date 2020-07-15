dbb8f0d09c35cf65b6793c3e11eea08c126eaf3b
taskj <taskj141@gmail.com>
2020-07-15 16:53:31 +0800

    添加多线程内容但未完成

--- 基于字典的目录资源探测工具/Dirbrute.py
+++ 基于字典的目录资源探测工具/Dirbrute.py
@@ -1,6 +1,8 @@
 import getopt
 import sys
 import math
+import threading
+import requests
 
 def init():
     start()
@@ -40,26 +42,53 @@ def multi_scan(url,threads,dic):
     #第一步读取字典文件
     #第二部确定读取的行数
     #第三部制作每一个线程读取的字典列表[[t1],[t2],[t3]]
-    result_list = []
+    result_list = [] #最终结果数组
+    temp_list = [] #临时数组
+    threads_list = [] #多线程列表
     with open("dic.txt","r") as f:
         dic_list = f.readlines()
-        if len(dic_list) % int(threads) == 0:
-            thread_read_line_num = int(len(dic_list) / int(threads))
+        #计算每个线程要读取行数的余数
+        remainder = len(dic_list) % int(threads)
+        #计算出每个临时数组的长度
+        if remainder == 0:
+            thread_read_line_num = len(dic_list) / int(threads)
         else:
             thread_read_line_num = math.ceil(len(dic_list) / int(threads))
-        i = 0
-        temp_list = []
-        for line in dic_list:
-            i = i + 1
-            if i % thread_read_line_num == 0:
-                temp_list.append(line.strip())
-                result_list.append(temp_list)
-                temp_list = []
-            else:
-                temp_list.append(line.strip())
 
-    print(len(dic_list) % int(threads))
+        #计算最后一个临时数组的长度
+        remainArr = int(len(dic_list) % thread_read_line_num)
+        i = 1
+        if remainArr == 0:
+            for line in dic_list:
+                if i == thread_read_line_num:
+                    temp_list.append(line.strip())
+                    result_list.append(temp_list)
+                    temp_list = []
+                    i = 1
+                else:
+                    temp_list.append(line.strip())
+                    i = i + 1
+        else:
+            for line in dic_list:
+                if i == thread_read_line_num:
+                    temp_list.append(line.strip())
+                    result_list.append(temp_list)
+                    temp_list = []
+                    i = 1
+                else:
+                    temp_list.append(line.strip())
+                    i = i + 1
+            result_list.append(temp_list)
 
+
+        for i in result_list:
+            threads_list.append(threading.Thread(target=scan, args=(url,i)))
+
+def scan(url,dic):
+    for line in dic:
+        r = requests.get(url + "/" + line)
+        if r.status_code == 200:
+            print(r.url + ":" + r.status_code)
 
 
 

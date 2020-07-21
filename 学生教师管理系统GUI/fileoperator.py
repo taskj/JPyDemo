03ddfbf97b3c47721957e7a6b1ec8d424d3973bc
taskj <taskj141@gmail.com>
2020-07-21 22:58:06 +0800

    新增文件读取操作

--- 学生教师管理系统GUI/fileoperator.py
+++ 学生教师管理系统GUI/fileoperator.py
@@ -13,7 +13,38 @@ class Files:
         self.list_teacher_all = []
 
     def read_from_file(self):
-        pass
+        #读取学生信息
+        try:
+            with open(self.student_path,encoding="utf-8",mode="r") as fd_student:
+                #逐行读取
+                current_line = fd_student.readline()
+                #判断是否有数据
+                while current_line:
+                    #分割成数组
+                    student_list = current_line.split(",")
+                    #直接添加
+                    self.list_student_all.append(student_list)
+                    #读取下一行
+                    current_line = fd_student.readline()
+        except Exception as e:
+            raise e
+        #读取教师信息
+        try:
+            with open(self.teacher_path,encoding="utf-8",mode="r") as fd_teacher:
+                #逐行读取
+                current_line = fd_teacher.readline()
+                #判断是否有数据
+                while current_line:
+                    #分割成数组
+                    teacher_list = current_line.split(",")
+                    #直接添加
+                    self.list_teacher_all.append(teacher_list)
+                    #读取下一行
+                    current_line = fd_teacher.readline()
+        except Exception as e:
+            raise e
 
 if __name__ == '__main__':
-    pass
+    file = Files()
+    file.read_from_file()
+    print(file.list_teacher_all)

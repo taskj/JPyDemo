0946666d0497c68829e427ee32fe22fabfac8305
taskj <taskj141@gmail.com>
2020-08-06 00:24:40 +0800

    新增正则表达式

--- 图像GPS信息读取/get_img_gps.py
+++ 图像GPS信息读取/get_img_gps.py
@@ -11,16 +11,27 @@ def find_gps_image(pic_path):
         for tag,value in tags.items():
             if re.match('GPS GPSLatitudeRef',tag):
                 #纬度
-                GPS['GPS GPSLatitudeRef'] = str(value)
+                GPS['GPSLatitudeRef'] = str(value)
             elif re.match('GPS GPSLongitudeRef',tag):
                 #经度
-                GPS['GPS GPSLongitudeRef'] = str(value)
+                GPS['GPSLongitudeRef'] = str(value)
             elif re.match('GPS GPSAltitudeRef',tag):
                 #海拔
-                GPS['GPS GPSAltitudeRef'] = str(value)
+                GPS['GPSAltitudeRef'] = str(value)
             elif re.match('GPS GPSAltitudeRef',tag):
                 #海拔
-                GPS['GPS GPSLatitude'] = str(value)
+                GPS['GPSLatitude'] = str(value)
+            elif re.match('GPS GPSLatitude', tag):
+                try:
+                    match_result = re.match('\[(\w*),(\w*),(\w.*)/(\w.*)\]',str(value)).group()
+                    GPS['GPSLatitude'] = int(match_result[0],match_result[1],match_result[2])
+                except:
+                    pass
+
+        #输出获得结果的键值对，如设备型号
+        for tag1,value1 in tags.items():
+            if tag1 == "Image Model":
+                print(value1)
 
 if __name__ == '__main__':
     find_gps_image('gps_test1.jpg')
